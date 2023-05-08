a37cff720661e98fa176433fd64bd7064d39fc8f
RafaGms <faelgomes2003@gmail.com>
2023-05-08 11:59:27 -0300

    Removed firebase credentials

--- config/firebase.js
+++ config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyA2okLf0f5rP-346KQQPgYN22jWX3GpyEA",
-    authDomain: "crud-firebase-expo-20f3e.firebaseapp.com",
-    projectId: "crud-firebase-expo-20f3e",
-    storageBucket: "crud-firebase-expo-20f3e.appspot.com",
-    messagingSenderId: "719927510524",
-    appId: "1:719927510524:web:d6f87a51562f60f1bd5e79"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
 
